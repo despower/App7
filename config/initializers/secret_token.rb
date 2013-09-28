e0b54175910718db56b247dc6a30bcb82d4cda6b
Des Power <despower1@mac.com>
2013-09-28 10:03:21 +0100

    Edited ignore file

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+App7::Application.config.secret_token = 'f68617980fc0f5e12d425c0dcf556658580817b0327d07f758b64900899f7e6cb8f385d62a9fdf8c49249959592b4ef6adcbb3f352cc29b7795420fb28681d2d'
