8410a6d246c536cd4f6bbe704e387ef056091e34
Des Power <despower1@mac.com>
2013-09-30 14:29:55 +0100

    added a root home and about pages plus some content

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 App7::Application.routes.draw do
+ get 'about' => 'pages#about'
+
+ root :to => 'pages#home'
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
